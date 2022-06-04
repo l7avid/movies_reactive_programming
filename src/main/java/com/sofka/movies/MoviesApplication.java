089ed0bf8b751f99497eb35883ad644b77f5b7d7
David Ernesto Reina Munar <davidereina@hotmail.com>
2022-06-04 17:26:04 -0500

    second usecase implemented on controller (PostMovieUseCase)

--- src/main/java/com/sofka/movies/MoviesApplication.java
+++ src/main/java/com/sofka/movies/MoviesApplication.java
@@ -1,7 +1,9 @@
 package com.sofka.movies;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class MoviesApplication {
@@ -10,4 +12,8 @@ public class MoviesApplication {
 		SpringApplication.run(MoviesApplication.class, args);
 	}
 
+	@Bean
+	public ModelMapper mapper(){
+		return new ModelMapper();
+	}
 }
--- src/main/java/com/sofka/movies/controller/MovieController.java
+++ src/main/java/com/sofka/movies/controller/MovieController.java
@@ -2,8 +2,10 @@ package com.sofka.movies.controller;
 
 import com.sofka.movies.DTO.MovieDTO;
 import com.sofka.movies.usecases.GetMoviesUseCase;
+import com.sofka.movies.usecases.PostMovieUseCase;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.reactive.function.BodyInserter;
 import org.springframework.web.reactive.function.BodyInserters;
@@ -16,9 +18,21 @@ import static org.springframework.web.reactive.function.server.RouterFunctions.r
 public class MovieController {
 
     @Bean
-    public RouterFunction<ServerResponse> allMovies(GetMoviesUseCase moviesUseCase){
+    public RouterFunction<ServerResponse> allMovies(GetMoviesUseCase getUseCase){
         return route(GET("/movies"), request -> ServerResponse.ok()
                 .contentType(MediaType.APPLICATION_JSON)
-                .body(BodyInserters.fromProducer(moviesUseCase.getMovies(), MovieDTO.class)));
+                .body(BodyInserters.fromProducer(getUseCase.getMovies(), MovieDTO.class)));
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> createMovie(PostMovieUseCase postUseCase){
+        return route(POST("/create/movie").and(accept(MediaType.APPLICATION_JSON)),
+                request -> request.bodyToMono(MovieDTO.class)
+                        //.flatMap(movieDTO -> postUseCase.postMovie(movieDTO)) same as below but using arrow function
+                        .flatMap(postUseCase::postMovie)
+                        .flatMap(movieDTO -> ServerResponse.status(HttpStatus.CREATED)
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .bodyValue(movieDTO))
+                        .onErrorResume(e -> ServerResponse.status(HttpStatus.BAD_REQUEST).build()));
     }
 }
